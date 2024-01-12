a25289b6066f18f6ce91f8ba52df74bb6e056943
Ubuntu <ubuntu@ip-172-31-53-110.ec2.internal>
2024-01-12 07:09:20 +0000

    config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  experimental: {
+    concurrentFeatures: true,
+    workerThreads: true,  
+  },
+  jest: {
+    // increase maxWorkers 
+    maxWorkers: 10,
+    // increase retries
+    retryTimes: 3,
+    // retry on failures
+    retryOnFailure: true 
+  }
+}
